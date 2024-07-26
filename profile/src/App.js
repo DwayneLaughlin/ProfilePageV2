74e150c87f784aaa7ba3095b3c151144ce4ebadd
dwayne.laughlin <dwayne.laughlin@fdmgroup.com>
2024-07-25 20:07:54 -0500

    added resume component

---
+++ profile/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Res from "./Resume.js";
+
+function App() {
+  return (
+    <div>
+      <Res></Res>
+    </div>
+  );
+}
+
+export default App;
---
+++ profile/src/Resume.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Resume from "./assets/Resume.pdf";
+
+class Res extends Component {
+  render() {
+    return (
+      <div>
+        <a href={Resume}>click</a>
+      </div>
+    );
+  }
+}
+export default Res;
